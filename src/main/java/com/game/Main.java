0bf8845e8108306a675bc4e9625b17f20ed841f0
童哥哥 <838103910@qq.com>
2017-04-14 11:44:31 +0800

    game commit

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,14 +1,95 @@
 
 package com.game;
 
+import com.game.util.OperUtil;
+import com.google.gson.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
+
+    private static Map<Integer,String> gameMap = new HashMap<Integer, String>();
+
 
     public static void main(String[] args) throws Exception {
 
-        /*String userId = "122831357";
-        String roomId = "117366944";
-        String up = "8I1J1D1212111K1D1K1I1ZPM8E1JYUJ3JYJZ1D121K7E";
-        String token = OperUtil.login(userId, up);*/
+        int userId = 7507411;
+        String up = "E2UOUEU2UZUEUKUMUMIDEZUOUMUMUMUMDMUMZZ";
+        String token = OperUtil.login(userId, up);
+        int[] a = new int[] {1,2,3};
+        Random randon = new Random();
+        int successCount = 0;
+        int failCount = 0;
+        int bet1 = 0;
+        int bet2 = 0;
+        while (true){
+            JsonObject game = OperUtil.getGameStatus(userId,token);
+            JsonObject gameInfo = game.get("gameInfo").getAsJsonObject();
+            int gameStatus = gameInfo.get("gameStatus").getAsInt();
+            int gameNo = gameInfo.get("gameNo").getAsInt();
+            long endTime = gameInfo.get("endTime").getAsLong();
 
+            if(gameStatus == 0 || gameStatus == 3){
+                Thread.sleep(endTime - System.currentTimeMillis());
+            }else if (gameStatus == 1){
+                bet1 = a[randon.nextInt(3)];
+                bet2 = a[randon.nextInt(3)];
+                gameMap.put(gameNo,bet1 + "_" + bet2);
+                OperUtil.playGame(gameNo, bet1,100,userId,token);
+                OperUtil.playGame(gameNo, bet2,100,userId,token);
+                Thread.sleep(endTime - System.currentTimeMillis());
+                /*if((endTime - System.currentTimeMillis()) > 2000){
+                    Thread.sleep(endTime - System.currentTimeMillis() - 2000);
+                }else {
+                    JsonObject betInfos = gameInfo.get("betInfos").getAsJsonObject();
+                    if(betInfos.has("betInfoArry")){
+                        JsonArray betInfoArry = betInfos.get("betInfoArry").getAsJsonArray();
+                        int length = betInfoArry.size();
+                        int totalBet1 = 0, totalBet2 = 0, totalBet3 = 0;
+                        for(JsonElement ele : betInfoArry){
+                            JsonObject obj = (JsonObject)ele;
+                            int country = obj.get("country").getAsInt();
+                            String totalBet = obj.get("totalBet").getAsString();
+                            if(country == 1){
+                                totalBet1 = Integer.valueOf(totalBet);
+                            }else if (country == 2){
+                                totalBet2 = Integer.valueOf(totalBet);
+                            }else if (country == 3){
+                                totalBet3 = Integer.valueOf(totalBet);
+                            }
+                        }
+                        if(totalBet1 > totalBet2 + totalBet3){
+                            OperUtil.playGame(gameNo,2,100,userId,token);
+                            OperUtil.playGame(gameNo,3,100,userId,token);
+                            Thread.sleep(endTime - System.currentTimeMillis());
+                        }else if (totalBet2 > totalBet1 + totalBet3){
+                            OperUtil.playGame(gameNo,1,100,userId,token);
+                            OperUtil.playGame(gameNo,3,100,userId,token);
+                            Thread.sleep(endTime - System.currentTimeMillis());
+                        }else if (totalBet3 > totalBet1 + totalBet2){
+                            OperUtil.playGame(gameNo,1,100,userId,token);
+                            OperUtil.playGame(gameNo,2,100,userId,token);
+                            Thread.sleep(endTime - System.currentTimeMillis());
+                        }
+
+                    }
+                }*/
+
+            }else if (gameStatus == 2){
+                String bet = gameMap.get(gameNo);
+                int result = gameInfo.get("gameResult").getAsInt();
+                if(bet != null){
+                    if(bet.contains(String.valueOf(result))){
+                        System.out.println("bet success : " + successCount++);
+                    }else{
+                        System.out.println("bet fail : " + failCount);
+                    }
+
+                }
+                Thread.sleep(endTime - System.currentTimeMillis());
+            }
+        }
     }
 }
